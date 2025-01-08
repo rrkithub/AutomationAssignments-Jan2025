e1123054bd666cf9ccce5561e09196c1483c42c7
lenovo <badarijaganadh@gmail.com>
2025-01-08 16:04:34 +0530

    Added ElectricityBill example assignment

--- src/test/java/JavaBasics/ElectricityBillnew.java
+++ src/test/java/JavaBasics/ElectricityBillnew.java
@@ -2,32 +2,39 @@ package JavaBasics;
 
 import java.util.Scanner;
 
-public class ElectricityBill {
+public class ElectricityBillnew {
 
     public static void main(String[] args) {
 
         // Create scanner object for taking input
-        Scanner scanner = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
         // Take input for the number of units consumed
 
         System.out.println("Enter the number of units consumed: ");
 
-        double units = scanner.nextDouble();
+        double units = sc.nextDouble();
 
         // Initialize the bill variable
 
         double bill = 0.0;
 
         // Calculate bill based on unit slabs
+
         if (units <= 50) {
             bill = units * 0.50;
-        } else if (units <= 200) {
-            bill = 50 * 0.50 + (units - 50) * 0.75;
-        } else if (units <= 450) {
-            bill = 50 * 0.50 + 150 * 0.75 + (units - 200) * 1.20;
+
+        } else if (units <= 150) {
+            bill = units* 0.75;
+
+        } else if (units <= 250) {
+            bill = units * 1.20;
+
+        } else if (units >= 250) {
+            bill = units * 1.50;
+
         } else {
-            bill = 50 * 0.50 + 150 * 0.75 + 250 * 1.20 + (units - 450) * 1.50;
+            bill = 50 * 0.50 + 150 * 0.75 + 250 * 1.20 + 250 * 1.50;
         }
 
         // Adding 20% surcharge
