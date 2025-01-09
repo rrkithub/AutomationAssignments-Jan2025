97e6fb3374de3787ce39ca55bfae63a6098b7fc1
lenovo <badarijaganadh@gmail.com>
2025-01-09 10:52:13 +0530

    Added 6 assignment

---
+++ src/test/java/JavaBasics/Terniaryopertor.java
@@ -0,0 +1,14 @@
+package JavaBasics;
+
+public class Terniaryopertor {
+    public static void main(String[] args) {
+        int n=5;
+        if (n%2 ==0){
+            System.out.println("Even number");
+        }
+        else {
+            System.out.println("Odd Number");
+        }
+        //System.out.println((n%2==0)?"Even Number":"Odd Number");
+    }
+}
